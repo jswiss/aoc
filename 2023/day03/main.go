7bca360eee8264fa409985e9409fcbe5feb76561
Joshua Swiss <joshua.swiss@skyscanner.net>
2023-12-06 13:08:15 +0000

    create map of symbol and indices

--- 2023/day03/main.go
+++ 2023/day03/main.go
@@ -7,17 +7,25 @@ import (
 	"github.com/jswiss/adventofcode/common/input"
 )
 
-func GetSymbolIndices(input string) []int {
+type Symbol struct {
+	line    int
+	symbols []int
+}
+
+var symbols []Symbol
+
+func GetSymbolIndices(idx int, input string) Symbol {
 	pattern := `[^a-zA-Z0-9.]`
-	indices := []int{}
 	re := regexp.MustCompile(pattern)
 
 	matches := re.FindAllStringIndex(input, -1)
 
+	s := Symbol{line: 0, symbols: []int{}}
+	s.line = idx
 	for _, arr := range matches {
-		indices = append(indices, arr[0])
+		s.symbols = append(s.symbols, arr[0])
 	}
-	return indices
+	return s
 
 }
 
@@ -29,13 +37,12 @@ func main() {
 	}
 
 	// loop through and get index of every symbol on each line
-	symbolIndices := []int{}
-	for _, val := range fileContent {
-		indices := GetSymbolIndices(val)
-		fmt.Println("Indices: ", indices)
-		symbolIndices = append(symbolIndices, indices...)
+	for i, val := range fileContent {
+		idx := i
+		indices := GetSymbolIndices(idx, val)
+		symbols = append(symbols, indices)
 	}
-	fmt.Println(symbolIndices)
+	fmt.Println(symbols)
 	// loop through input and get the index and value of numbers on each line
 	// get valid numbers per line
 	// get valid numbers compared with symbol index on previous line  (diagonal)
--- 2023/day03/main_test.go
+++ 2023/day03/main_test.go
@@ -8,9 +8,13 @@ import (
 // TestHelloName calls greetings.Hello with a name, checking
 // for a valid return value.
 func TestSymbolIndices(t *testing.T) {
+	idx := 0
 	line := ".*.$.aaa...555"
-	want := []int{1, 3}
-	got := GetSymbolIndices(line)
+	var symbol Symbol
+	symbol.line = 0
+	symbol.symbols = []int{1, 3}
+	want := symbol
+	got := GetSymbolIndices(idx, line)
 
 	if reflect.DeepEqual(want, got) == false {
 		t.Fatalf(`Yo, this ain't working. You wanted %v, but you got %v`, want, got)
